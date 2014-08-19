17087cce8f6353b0258cc9e7ee5b348b021a6de7
Sang Han <jjangsangy@gmail.com>
2014-08-19 05:09:09 -0700

    general fibonacci implmenetation

---
+++ C/fibonacci.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <math.h>
+
+long fibonacci(int n) {
+    return lround((
+        pow(0.5 + 0.5 * sqrt(5.0), n)  -
+        pow(0.5 - 0.5 * sqrt(5.0), n)) /
+            sqrt(5.0));
+}
+
+int main(void)
+{
+    int i, n=50;
+    for (i=0; i<=n; i++)
+        printf(
+            "Fibonacci of %02i is equal to %ld\n", i, fibonacci(i));
+
+    return 0;
+}
