03d2ebb6ccae0181f5735ecc6c8bf72aae4b46ae
Sang Han <jjangsangy@gmail.com>
2014-08-18 19:31:44 -0700

    generalized solution

--- Python/004-largest_palindrome_product.py
+++ Python/004-largest_palindrome_product.py
@@ -9,6 +9,7 @@ Find the largest palindrome made from the product of two 3-digit numbers.
 """
 
 def is_palindrome(string):
+    """ Brute force algorithm """
     decide, i = 1, 0
     while (i<=int(len(string)/2) and decide==1):
         if string[i] != string[-(i+1)]:
@@ -16,10 +17,13 @@ def is_palindrome(string):
         i+=1
     return decide
 
-if __name__ == '__main__':
+def main(digits):
     aux=0
-    for k in range(101,1000):
-        for j in range(101,1000):
-            if (is_palindrome(str(j*k)) and j*k>aux):
-                aux=j*k
-    print(aux)
+    for i in range(10^(digits-1)+1, 10**digits):
+        for j in range(10^(digits-1)+1, 10**digits):
+            if (is_palindrome(str(j*i)) and i*j>aux):
+                aux=i*j
+    return aux
+
+if __name__ == '__main__':
+    print(main(3))
