33a4c120ba7bcaa9b4cb9397ac8ff2418274d4d0
Sang Han <jjangsangy@gmail.com>
2014-08-18 22:11:09 -0700

    len always returns int

--- Python/004-largest_palindrome_product.py
+++ Python/004-largest_palindrome_product.py
@@ -11,7 +11,7 @@ Find the largest palindrome made from the product of two 3-digit numbers.
 def is_palindrome(string):
     """ Brute force algorithm """
     i, palindrome = 0, True
-    while i<=int(len(string)/2) and palindrome == True:
+    while i<=len(string)/2 and palindrome == True:
         if string[i] != string[-(i+1)]:
             palindrome = False
         i+=1
