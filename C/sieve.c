fb6eb1fa7ff6d8c7029e5db8bdef8a3b484686f7
jjangsangy <jjangsangy@gmail.com>
2014-11-03 23:58:15 -0800

    sieve

--- C/sieve.c
+++ C/sieve.c
@@ -1,30 +1,47 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
 
-long *sieve(long limit) {
+long *sieve(int limit)
+{
     long long i, j;
     long *primes = malloc(limit * sizeof(long));
-    
-    for (i=2; i<limit; i++) primes[i] = 1;
-    
-    // Enumerate through multiples
+
+    // Fill Array
+    for (i=2; i<limit; i++)
+        primes[i] = 1;
+
+    // Sieve Away
     for (i=2; i<limit; i++)
         if (primes[i])
             for (j=i; i*j<limit; j++)
                 primes[i*j] = 0;
-    
+
     return primes;
+}
+
+int is_prime(int n)
+{
+    int i;
+    for (i=2; i<n; i--)
+        if (n % i == 1)
+            return true;
+    return false;
 }
 
 int main(int argc, char *argv[])
 {
-    long max = 100;
+    int i, max = atoi(argv[1]);
     long *primes = sieve(max);
-    
-    int i;
-    for (i=1; i<100; i++)
-        if (*(primes+i) == 0)
-            printf("%i\n", i);
-    
+
+    // Print Out Primes
+    for (i=3; i<max; i++)
+        if (primes[i] == 1)
+            printf("Prime: %i Check: %s\n", i,
+                is_prime(i) ? "True": "False"
+            );
+
     free(primes);
+
     return 0;
 }
