35591dabec40edaf8e960953a296ba8f1042f7c9
jjangsangy <jjangsangy@gmail.com>
2014-11-15 07:44:56 -0800

    prime number sieves

--- Python/sieve.py
+++ Python/sieve.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 def set_sieve(n):
     """
     Sets are mutable, unordered collections which are useful
@@ -8,3 +10,33 @@ def set_sieve(n):
         if i in primes:
             primes -= set(range(i*i, n+1, i))
     return primes
+
+
+def sieve(n):
+    """Sieve away and only primes are left."""
+    primes = 2 * [False] + (n - 1) * [True]
+    for i in range(2, int(n**0.5+1)):
+        for j in range(i*i, n+1, i):
+            primes[j] = False
+    return [prime for prime, checked in enumerate(primes) if checked]
+
+
+def set_insertion_sieve(n):
+    """Performing insertion over deletion"""
+    factors = set()
+    for i in range(2,int(n**0.5+1)):
+        if i not in factors:
+            factors |= set(range(i*i, n+1, i))
+    return set(range(2,n+1)) - factors
+
+
+def np_sieve(n):
+    """
+    Sieve with it's guts swapped with numpy
+    ndarray
+    """
+    primes = np.ones(n+1, dtype=np.bool)
+    for i in np.arange(2, n**0.5+1, dtype=np.uint32):
+        if primes[i]:
+            primes[i*i::i] = False
+    return np.nonzero(primes)[0][2:]
